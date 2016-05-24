6367fad0430fecdf70e8cbb8d8fedde68a62aedc
Jafet95 <jafet.a15@gmail.com>
2016-05-23 21:18:56 -0600

    Actualización 23/05/16
    Cambios menores en los bits designados para las constantes de las
    ubicaciones de las imágenes

--- generador_imagenes.v
+++ generador_imagenes.v
@@ -29,50 +29,50 @@ output wire [7:0] pic_RGB
 
 //Declaración de constantes
 //Imagen HORA
-localparam pic_hora_XL = 13'd256; //Límite izquierdo
-localparam pic_hora_XR = 384; //Límite derecho
-localparam pic_hora_Y = 13'd64; //Límite inferior
+localparam pic_hora_XL = 9'd256; //Límite izquierdo
+localparam pic_hora_XR = 9'd384; //Límite derecho
+localparam pic_hora_Y = 7'd64; //Límite inferior
 localparam hora_size = 14'd8192;//Cuantas líneas de texto se deben leer (valor se encuentra en Matlab, variable COLOR_HEX)
-localparam pic_hora_XY = 13'd64;
+localparam pic_hora_XY = 7'd64;
 
 //Imagen FECHA
-localparam pic_fecha_XL = 16'd128; //Límite izquierdo
-localparam pic_fecha_XR = 208; //Límite derecho
-localparam pic_fecha_YT = 416;	//Límite superior
-localparam pic_fecha_YB = 16'd448; //Límite inferior
-localparam pic_fecha_size = 16'd2560;// (80x32)
-localparam pic_fecha_XY = 16'd32;
+localparam pic_fecha_XL = 8'd128; //Límite izquierdo
+localparam pic_fecha_XR = 8'd208; //Límite derecho
+localparam pic_fecha_YT = 9'd416;	//Límite superior
+localparam pic_fecha_YB = 9'd448; //Límite inferior
+localparam pic_fecha_size = 12'd2560;// (80x32)
+localparam pic_fecha_XY = 6'd32;
 
 //Imagen TIMER
-localparam pic_timer_XL = 16'd416; //Límite izquierdo
-localparam pic_timer_XR = 496; //Límite derecho
-localparam pic_timer_YT = 416;	//Límite superior
-localparam pic_timer_YB = 16'd448; //Límite inferior
-localparam pic_timer_size = 16'd2560;// (80x32)
-localparam pic_timer_XY = 16'd32;
+localparam pic_timer_XL = 9'd416; //Límite izquierdo
+localparam pic_timer_XR = 9'd496; //Límite derecho
+localparam pic_timer_YT = 9'd416;	//Límite superior
+localparam pic_timer_YB = 9'd448; //Límite inferior
+localparam pic_timer_size = 12'd2560;// (80x32)
+localparam pic_timer_XY = 6'd32;
 
 //Imagen RING
-localparam pic_ring_XL = 16'd512; //Límite izquierdo
-localparam pic_ring_XR = 639; //Límite derecho
-localparam pic_ring_YT = 128;	//Límite superior
-localparam pic_ring_YB = 16'd192; //Límite inferior
-localparam pic_ring_size = 16'd8192;// (128x64)
-localparam pic_ring_XY = 16'd64;
+localparam pic_ring_XL = 10'd512; //Límite izquierdo
+localparam pic_ring_XR = 10'd639; //Límite derecho
+localparam pic_ring_YT = 8'd128;	//Límite superior
+localparam pic_ring_YB = 8'd192; //Límite inferior
+localparam pic_ring_size = 14'd8192;// (128x64)
+localparam pic_ring_XY = 7'd64;
 
 //Imagen RING ball
-localparam pic_ringball_XL = 16'd544; //Límite izquierdo
-localparam pic_ringball_XR = 592; //Límite derecho
-localparam pic_ringball_YT = 64;	//Límite superior
-localparam pic_ringball_YB = 16'd112; //Límite inferior
-localparam pic_ringball_size = 16'd2304;// (48x48)
-localparam pic_ringball_XY = 16'd48;
+localparam pic_ringball_XL = 10'd544; //Límite izquierdo
+localparam pic_ringball_XR = 10'd592; //Límite derecho
+localparam pic_ringball_YT = 7'd64;	//Límite superior
+localparam pic_ringball_YB = 7'd112; //Límite inferior
+localparam pic_ringball_size = 12'd2304;// (48x48)
+localparam pic_ringball_XY = 6'd48;
 
 //Imagen LOGO
-localparam pic_logo_XL = 16'd0; //Límite derecho
-localparam pic_logo_XR = 128; //Límite derecho
-localparam pic_logo_YB = 16'd16; //Límite inferior
-localparam pic_logo_size = 16'd2048;// (128x16)
-localparam pic_logo_XY = 16'd16;
+localparam pic_logo_XL = 12'd0; //Límite derecho
+localparam pic_logo_XR = 8'd128; //Límite derecho
+localparam pic_logo_YB = 5'd16; //Límite inferior
+localparam pic_logo_size = 12'd2048;// (128x16)
+localparam pic_logo_XY = 5'd16;
 
 //Declaración de señales
 reg [7:0] colour_data_hora [0:hora_size-1];	//datos de los colores
@@ -81,12 +81,12 @@ reg [7:0] colour_data_timer [0:pic_timer_size-1];	//datos de los colores
 reg [7:0] colour_data_ring [0:pic_ring_size-1];	//datos de los colores
 reg [7:0] colour_data_ringball [0:pic_ringball_size-1];	//datos de los colores
 reg [7:0] colour_data_logo [0:pic_logo_size-1];	//datos de los colores
-wire [15:0] STATE_hora; //Bits dependen de hora_size
-wire [15:0] STATE_fecha; //Bits dependen de hora_size
-wire [15:0] STATE_timer; //Bits dependen de hora_size
-wire [15:0] STATE_ring; //Bits dependen de hora_size
-wire [15:0] STATE_ringball; //Bits dependen de hora_size
-wire [15:0] STATE_logo; //Bits dependen de hora_size
+wire [13:0] STATE_hora; //Bits dependen de hora_size
+wire [11:0] STATE_fecha; //Bits dependen de hora_size
+wire [11:0] STATE_timer; //Bits dependen de hora_size
+wire [13:0] STATE_ring; //Bits dependen de hora_size
+wire [11:0] STATE_ringball; //Bits dependen de hora_size
+wire [11:0] STATE_logo; //Bits dependen de hora_size
 wire pic_hora_on, pic_fecha_on, pic_timer_on, pic_logo_on;
 reg [7:0] pic_RGB_aux;
 
