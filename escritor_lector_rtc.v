26901009b8ee54c8855252020867b4f0a2601002
yermyb9 <yermyb9@gmail.com>
2016-05-26 21:30:12 -0600

    actualizacion 26/5/16

--- escritor_lector_rtc.v
+++ escritor_lector_rtc.v
@@ -20,58 +20,111 @@
 //////////////////////////////////////////////////////////////////////////////////
 module escritor_lector_rtc(
 	input clk,reset,
-	input [7:0]port_id,reg_direccion_escribir,reg_dato_escribir,
+	input [7:0]port_id,in_dato,
 	input write_strobe,read_strobe,
-	output [7:0]reg_dato_leido,
-	output [7:0]flag_done,
-	inout dato
+	output reg_a_d,reg_cs,reg_rd,reg_wr,
+	output reg[7:0]out_dato,
+	output fin_lectura_escritura,
+	inout [7:0]dato
+	
     );
-	 
+ 
+wire flag_done;
+assign fin_lectura_escritura = flag_done;
+reg en_funcion;
+reg [7:0]addr_RAM,dato_escribir;
+wire [7:0]dato_leido;
+reg [7:0]reg_addr_RAM, reg_dato_escribir,reg_dato_leido;
+reg reg_escribir_leer,escribir_leer;
+wire direccion_dato;
 /// I/O Datos
-
-Driver_bus_bidireccional instance_name (
+Driver_bus_bidireccional instance_driver_bus_bidireccional (
     .in_flag_escritura(~reg_wr), 
     .in_flag_lectura(~reg_rd), 
+    .in_direccion_dato(direccion_dato), 
     .in_dato(dato_escribir), 
-    .out_dato(dato_leido), 
+    .out_reg_dato(dato_leido),  
+    .addr_RAM(addr_RAM), 
     .dato(dato)
     );
-
 
 //Generador de señales de control
 signal_control_rtc_generator instance_signal_control_rtc_generator (
     .clk(clk), 
     .reset(reset), 
-    .in_escribir_leer(in_escribir_leer), 
+    .in_escribir_leer(escribir_leer), 
     .en_funcion(en_funcion), 
     .reg_a_d(reg_a_d), 
     .reg_cs(reg_cs), 
     .reg_wr(reg_wr), 
     .reg_rd(reg_rd), 
-    .out_direccion_dato(out_direccion_dato), 
+    .out_direccion_dato(direccion_dato), 
     .flag_done(flag_done)
     );
-
-reg en_funcion;
-reg [7:0]reg_address,dato_escribir,
+reg reg_flag_done,next_flag_done;
 always@(posedge clk) begin
-	if ()
+	reg_flag_done = next_flag_done; 
+end
+always@(*)begin
+	//case({flag_done,read_strobe,port_id == 8'h11})
+		//3'b100: next_flag_done = 1'b1;
+end
+// logica secuencial
+always@(posedge clk , posedge reset) begin
+	if (reset)begin
+		addr_RAM <= 8'h0;
+		dato_escribir <= 8'h0;
+		escribir_leer <= 1'b0;
+	end
+	else begin
+		addr_RAM <= reg_addr_RAM;
+		dato_escribir <= reg_dato_escribir;
+		escribir_leer <= reg_escribir_leer;
+		
+	end
 end
 
 // logica combinacional para port_id
 always@* begin
-	case (port_id)
-	8'h00: begin 
-	en_funcion = 1'b1;
-	reg_address = reg_direccion_escribir;
-	end
-	8'h01:begin
+	if ( write_strobe == 1'b1 || read_strobe == 1'b1) begin
+	// secuencia para sacar el dato recibido del rtc
+	if(port_id == 8'h12)out_dato = dato_leido;
+	else out_dato = 8'h00;
+	// inicio de secuencia de lectura_escritura rtc
+	if(port_id == 8'h10) en_funcion = 1'b1;
+	else en_funcion = 1'b0;
 	
+	case (port_id)
+	8'h00: begin //actualiza direccion
+	reg_addr_RAM = in_dato;
+	reg_dato_escribir = dato_escribir; 
+	reg_escribir_leer = escribir_leer;
 	end
-	endcase
+	8'h01: begin // actualiza dato
+	reg_dato_escribir = in_dato;
+	reg_addr_RAM = addr_RAM;
+	reg_escribir_leer = escribir_leer;
+	end
+	8'h10: begin // inicia secuancia de rtc
+	reg_addr_RAM = addr_RAM;
+	reg_dato_escribir = dato_escribir;
+	reg_escribir_leer = in_dato[0];
+	end
+	default: begin
+	reg_addr_RAM = addr_RAM;
+	reg_dato_escribir = dato_escribir;
+	reg_escribir_leer = escribir_leer;
+	end
+	endcase	
+	end
+	
+	else begin
+	reg_addr_RAM = addr_RAM;
+	reg_dato_escribir = dato_escribir;
+	reg_escribir_leer = escribir_leer;
+	end
 	
 end
-
 
 
 endmodule
